aae0833b0e9fef595661e7f004312ec498a8d360
it-learning-diary <rongbincheng@szbit.cn>
2022-04-24 18:18:57 +0800

    集成导入

---
+++ it-wheels-emp-imp-util/src/main/java/cn/it/learning/constant/ExportConstant.java
@@ -0,0 +1,32 @@
+package cn.it.learning.constant;
+
+/**
+ * @Author it-learning
+ * @Description 导出常量类
+ * @Date 2022/4/24 10:45
+ * @Version 1.0
+ */
+public class ExportConstant {
+    /**
+     * MIME类型
+     */
+    public static final String EXCEL_CONTENT_TYPE = "application/octet-stream";
+    /**
+     * 导出的编码类型
+     */
+    public static final String UTF_8 = "UTF-8";
+    public static final String CONTENT_DISPOSITION = "Content-Disposition";
+    /**
+     * 设置浏览器以附件的形式读取导出的文件
+     */
+    public static final String ATTACHMENT_FILENAME = "attachment;filename=";
+    /**
+     * 导出的文件后缀
+     */
+    public static final String XLSX_SUFFIX = ".xlsx";
+
+    /**
+     * 导出的sheet页命名
+     */
+    public static final String SHEET_NAME = "sheet";
+}
---
+++ it-wheels-emp-imp-util/src/main/java/cn/it/learning/util/ExcelImportUtil.java
@@ -0,0 +1,11 @@
+package cn.it.learning.util;
+
+/**
+ * @Author it-learning-diary
+ * @Description 导入excel模板
+ * @Date 2022/4/24 14:09
+ * @Version 1.0
+ */
+public class ExcelImportUtil {
+
+}
