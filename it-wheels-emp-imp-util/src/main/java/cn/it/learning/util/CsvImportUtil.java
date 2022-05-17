2ec697471923bff5c93fda0eb6a849290638affc
it-learning-diary <elvis.rong>
2022-05-17 23:13:26 +0800

    csv导入轮子基础代码

---
+++ it-wheels-emp-imp-util/src/main/java/cn/it/learning/util/CsvImportUtil.java
@@ -0,0 +1,25 @@
+package cn.it.learning.util;
+
+
+import com.univocity.parsers.csv.CsvParser;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+/**
+ * @Author it-learning
+ * @Description csv文件导入工具包
+ * @Date 2022/5/16 15:55
+ * @Version 1.0
+ */
+public class CsvImportUtil<T> {
+
+    public static void importCsvWithString(MultipartFile file) {
+
+    }
+
+
+    public static void importCsvWithBean(MultipartFile file) {
+
+    }
+}
